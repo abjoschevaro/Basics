cc4b488fd41a871a4538f79c63405ac183e40131
abjoschevaro <abrahamchevarria7@gmail.com>
2020-11-21 16:40:16 -0500

    On branch debug  Changes to be committed: 	modified:   programa.py

--- programa.py
+++ programa.py
@@ -3,5 +3,10 @@ def main():
 	Esto es un metodo escrito desde debug branch
 	le haré un merge para testear los resultados
 	*/
+
+//ahora edito debug branch, para luego ir a main y hacer un merge
+//Seguidamente, ver el historial de cambios de versiones
+//command line: git log --all --graph
 if __name__=="__main__":
 	main()
+
